592d1dae53a91185a87565695aa5092b4319dfa6
6ixspeed6 <6card3077@gmail.com>
2022-09-30 12:32:37 +0300

    burg-fix

--- js/index.js
+++ js/index.js
@@ -8,17 +8,20 @@ function burger() {
       const cross = document.querySelector('[data-burger-cross]')
       const main = document.querySelector('[data-burger-main]')
       const blackout = document.querySelector('[data-burger-blackout]')
+      const tagBody = document.querySelector('body')
 
       //кнопка
       btn.addEventListener('click', (event) => {
          main.classList.add('_active')
          blackout.classList.add('_active')
+         tagBody.classList.add('_lock-scroll')
       })
 
       //кнопка
       cross.addEventListener('click', (event) => {
          main.classList.remove('_active')
          blackout.classList.remove('_active')
+         tagBody.classList.remove('_lock-scroll')
       })
 
       //сенсорное закрытие
@@ -29,6 +32,7 @@ function burger() {
          } else {
             main.classList.remove('_active')
             blackout.classList.remove('_active')
+            tagBody.classList.remove('_lock-scroll')
          }
       })
 
